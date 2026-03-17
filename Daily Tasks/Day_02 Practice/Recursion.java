4cd82526498672301e4d332599476bd1e5c5e02b
KAVIN R <kavincs2006@gmail.com>
2026-03-17 19:13:11 +0530

    Update Recursion.java

--- Daily Tasks/Day_02 Practice/Recursion.java
+++ Daily Tasks/Day_02 Practice/Recursion.java
@@ -5,7 +5,7 @@ public class Main {
         return 1;
         return n*fact(n-1);
     }
-    public static void main(String[] args) {
+    public static void main(String args[]) {
     Scanner sc=new Scanner(System.in);
     int n=sc.nextInt();
     System.out.print(fact(n));
